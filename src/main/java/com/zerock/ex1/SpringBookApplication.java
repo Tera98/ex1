aa1c21f7c47fc98bb01a5a36f9573039b14f98ee
wonyong <awy0626@gmail.com>
2023-03-02 23:37:08 +0900

    Project 생성

---
+++ src/main/java/com/zerock/ex1/SpringBookApplication.java
@@ -0,0 +1,13 @@
+package com.zerock.ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBookApplication.class, args);
+    }
+
+}
